701f69bcd787cfef7569b91d820904002a8748bb
Mark Greenall <mark@markgreenall.co.uk>
2015-04-05 13:10:37 +0100

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,9 @@
+<?php
+spl_autoload_register(function($className)
+{
+    $class = __DIR__ . '/' . str_replace("FISH\\", '', $className) . '.php';
+
+    # Check if Class Exists & Include
+    if (file_exists($class))
+        require($class);
+});
